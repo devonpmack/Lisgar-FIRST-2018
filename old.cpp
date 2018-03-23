fe686fc32c0783a3ba2937d544caaef68a94db54
Lisgar IEEE Robotics <lisgarieeerobotics@gmail.com>
2018-03-23 15:37:42 -0400

    We decided to improve the code

--- old.cpp
+++ old.cpp
@@ -6,31 +6,34 @@
 /*----------------------------------------------------------------------------*/
 
 #include "WPILib.h"
-#include <iostream>
+#include <iostream>                       // input and output stream library, this includes various input and output
+                                          // functions which print to the console. The printing functionalities help
+                                          // us to debug the code.
 
 // set pins
-#define PIN_L_WHEEL       1
-#define PIN_R_WHEEL       0
-#define PIN_L_CLAW        2
-#define PIN_R_CLAW        3
-#define PIN_ARM           4
+#define PIN_L_WHEEL       1               // pin (L) wheel
+#define PIN_R_WHEEL       0               // pin (R) wheel
+#define PIN_L_CLAW        2               // claw (L) pin
+#define PIN_R_CLAW        3               // claw (R) pin
+#define PIN_ARM           4               // arm pin
 
-#define PIN_SERVO1        5
-#define PIN_SERVO2        6
+#define PIN_SERVO1        5               // servo pin 2
+#define PIN_SERVO2        6               // servo pin 1
 
-#define B_ARM_UP          2
-#define B_ARM_DOWN        1
-#define B_RESETCAM        7
+#define B_ARM_UP          2               // arm up button
+#define B_ARM_DOWN        1               // arm down button
+#define B_RESETCAM        7               // reset camera button
 
-#define AUTO_DRIVE_TIME   2.0       // seconds
-#define AUTO_DRIVE_SPEED  0.7
-#define AUTO_TURN_TIME    0.5     // seconds
-#define AUTO_FINAL_TIME   1 //seconds
-#define AUTO_TURN_SPEED   0.6
+#define POSITION          1               // starting position (1 = left, 2 = middle, 3 = right)
 
-#define AUTO_DRIVE_TIME_R   0.5       // seconds
-#define AUTO_TURN_TIME_R    0.5     // seconds
-#define AUTO_FINAL_TIME_R   2 //seconds
+#define AUTO_DRIVE_TIME   2.0             // seconds
+#define AUTO_DRIVE_SPEED  0.7             // relative power
+#define AUTO_TURN_TIME    0.5             // seconds
+#define AUTO_FINAL_TIME   1               // seconds
+#define AUTO_TURN_SPEED   0.6             // relative power
+
+#define MOVESPEED    1                    // relative power
+#define TURNSPEED    0.9                  // relative power
 
 class Robot : public frc::IterativeRobot {
 
@@ -64,8 +67,8 @@ class Robot : public frc::IterativeRobot {
 
 	// get the game data
 	std::string gameData;
-	int position = 3;
 	char switchSide;
+	bool done[4]; // this is ghetto but im feeling lazy
 	double servoX = 0.5;
 	double servoY = 0.5;
 public:
@@ -74,20 +77,22 @@ public:
 
 	// runs on robot initialization
 	void RobotInit() {
-		//does nothing? Hopefully you can use this to set the starting position
-		position = SmartDashboard::GetNumber("Position", 1);
 		// set up camera
-		//CameraServer::GetInstance()->StartAutomaticCapture();
+		CameraServer::GetInstance()->StartAutomaticCapture();
+		// get the game data
 
+
+		done[0] = false; //used to print out data in autophase
+		done[1] = false;
+		done[2] = false;
+		done[3] = false;
 	}
 
 	void AutonomousInit() {
 		autoTimer->Start();
 		autoTimer->Reset();
 
-		// get the game data
 		gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
-
 		// set the switch side (X if not detected)
 		if (gameData.length() > 0) {
 			switchSide = gameData[0];
@@ -95,9 +100,12 @@ public:
 			switchSide = 'X';
 		}
 
-		SmartDashboard::PutString("Sides (closest to farthest)", gameData);
-		SmartDashboard::PutBoolean("Our switch colour is on the LEFT?",switchSide == 'L');
-
+		if (switchSide == 'l') {
+			switchSide = 'L';
+		}
+		if (switchSide == 'r') {
+			switchSide = 'R';
+		}
 	}
 	void AutonomousPeriodic() {
 		auto_move();
@@ -108,13 +116,13 @@ public:
 	void TeleopPeriodic() {
 
 		// drive the robot with the first joystick
-		m_robotDrive.ArcadeDrive(-m_stick1->GetY(), m_stick1->GetX() * 0.7);
+		m_robotDrive.ArcadeDrive(-m_stick1->GetY() * MOVESPEED, m_stick1->GetX() * TURNSPEED);
 
 		// drive the claws with the second joystick
 		//m_clawDrive.ArcadeDrive(m_stick2.GetY(), m_stick2.GetX());
 		m_clawDrive.ArcadeDrive(m_stick2->GetZ(), m_stick2->GetX());
 		int pos = m_stick1->GetPOV(0);
-		//std::cout << pos << std::endl;
+		std::cout << pos << std::endl;
 		if (pos != -1) {
 			switch(pos) {
 				case 90:
@@ -139,6 +147,7 @@ public:
 		}
 		sv_1->Set(servoX);
 		sv_2->Set(servoY);
+		//Wait(1);
 
 		// grab the claw with the joystick trigger
 		if (m_stick2->GetRawButton(B_ARM_UP)) {
@@ -154,66 +163,64 @@ public:
 		}
 	}
 
-	/***********************/
-	/* AUTONOMOUS COMMANDS */
-	/***********************/
+	/*******************
+	 * AUTONOMOUS COMMANDS
+	 *******************/
 	void auto_move() {
-		SmartDashboard::PutNumber("Seconds Remaining",15-autoTimer->Get());
 		m_armMotor->SetSpeed(-0.5);
 
-		if (position == 1) {
-			// LEFT SIDE AUTO PHASE
+		if (!done[0]) {
+			std::cout << autoTimer->Get() << std::endl;
+		}
 
-			// move forward for AUTO_DRIVE_TIME seconds
+		// drive time
+		if (autoTimer->Get() < AUTO_DRIVE_TIME) {
+			if (!done[0]) {
+				std::cout << "fwd" << std::endl;
+				done[0] = true;
+			}
 			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0); //10.72.67.2 rio ip
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(0, -AUTO_TURN_SPEED);
-			frc::Wait(AUTO_DRIVE_TIME);
+		}
+		// turn time
+		else if (autoTimer->Get() < (AUTO_TURN_TIME + AUTO_DRIVE_TIME)) {
+			if (!done[1]) {
+				std::cout << "turn" << std::endl;
+				done[1] = true;
+			}
+			// Left side
+
+			if (switchSide == 'L') {
+				//Go for it
+				m_robotDrive.ArcadeDrive(-AUTO_TURN_SPEED, AUTO_TURN_SPEED);
+			}
+//			if (POSITION == 3 && switchSide == 'R') {
+//				//Go for right side
+//				m_robotDrive.ArcadeDrive(AUTO_TURN_SPEED, -AUTO_TURN_SPEED);
+//			}
+
+		} else if (autoTimer->Get() < (AUTO_TURN_TIME + AUTO_DRIVE_TIME + AUTO_FINAL_TIME)) {
+			if (!done[2]) {
+				std::cout << "finalfwd" << std::endl;
+				done[2] = true;
+			}
 			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
+		}
+		// eject time
+		else {
+			//hack
+			if (!done[3]) {
+				std::cout << "eject" << std::endl;
+				done[3] = true;
+			}
 			m_robotDrive.ArcadeDrive(0, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
 			if (switchSide == 'L') {
 				m_clawDrive.ArcadeDrive(-1, 0);
 			}
-			frc::Wait(AUTO_DRIVE_TIME);
-		}
 
-		if (position == 3) {
-			// RIGHT SIDE AUTO PHASE
-			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0); //10.72.67.2 rio ip
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(0, -AUTO_TURN_SPEED);
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(0, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
-			if (switchSide == 'R') {
-				m_clawDrive.ArcadeDrive(-1, 0);
-			}
-			frc::Wait(AUTO_DRIVE_TIME);
-		}
-
-		if (position == 2) {
-			if (switchSide == 'R') {
-				m_robotDrive.ArcadeDrive(0, AUTO_TURN_SPEED);
-			}
-			if (switchSide == 'L') {
-				m_robotDrive.ArcadeDrive(0,-AUTO_TURN_SPEED);
-			}
-			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0); //10.72.67.2 rio ip
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(0, -AUTO_TURN_SPEED);
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(AUTO_DRIVE_SPEED, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_robotDrive.ArcadeDrive(0, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
-			m_clawDrive.ArcadeDrive(-1, 0);
-			frc::Wait(AUTO_DRIVE_TIME);
 		}
 	}
+
+
 };
 
 START_ROBOT_CLASS(Robot)
